7ca3799ef02bd74263468a3b72e801e1848c8ffa
pierrBla <pierrcalderon6@hotmail.com>
2024-08-07 12:04:35 -0500

    Add DataContext

---
+++ Zulu.Api/Data/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Zulu.Shared.Entities;
+
+namespace Zulu.Api.Data
+{
+    //tenemos que eredar DbContext
+    //instalamos el  entityFremeworkcore
+    public class DataContext:DbContext
+    {
+        //crear ctor =contructor pasale DbContextOptions<DataContext>options
+        public DataContext(DbContextOptions<DataContext>options):base(options) 
+        
+        {   
+
+        }
+        //crear una propieda para mapear las tablas 
+        public DbSet<Country> Countries { get; set; }
+
+        //para que no vea duplicado
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(x=>x.Name).IsUnique();
+        }
+
+    }
+
+    }
+
---
+++ Zulu.Shared/Entities/Country.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Zulu.Shared.Entities
+{
+    public class Country
+
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Pais")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(100, ErrorMessage = "El Campo {0} no puede tener mas de {1} caractéres")]
+
+
+        public string Name { get; set; } = null!;
+
+
+    }
+}
