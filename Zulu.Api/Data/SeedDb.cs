bbc2b4872e88653b5a0d8900ae29a1ee79ef01ef
pierrBla <pierrcalderon6@hotmail.com>
2024-09-12 11:37:59 -0500

    SeedDB V1 Datos carcodeado

---
+++ Zulu.Api/Data/SeedDb.cs
@@ -0,0 +1,37 @@
+﻿namespace Zulu.Api.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+
+        public SeedDb(DataContext context)
+        {
+            _context = context;
+        }
+        //creamos un metodo async
+        //para poblar la base de datos
+
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+            //para checkear si hay paises
+            await checkCountriesAsync();
+
+        }
+
+        private async Task checkCountriesAsync()
+        {
+            //el metodo any devuelve tru si hay un registro
+            if(!_context.Countries.Any()) 
+            {
+                //ACA MARCAMOS
+                _context.Countries.Add(new Shared.Entities.Country { Name = "PERU" });
+                _context.Countries.Add(new Shared.Entities.Country { Name = "COLOMBIA" });
+                _context.Countries.Add(new Shared.Entities.Country { Name = "MEXICO" });
+
+                //ACA GUARDAMOS
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
--- Zulu.Api/Program.cs
+++ Zulu.Api/Program.cs
@@ -8,10 +8,38 @@ builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=cn"));
+builder.Services.AddTransient<SeedDb>();
+
+//tres fromas para inyectar servicio
+//builder.AddSingleton->  esto  permanece en momoria todo el tiempo
+//builder.Services.AddScoped ->cada que lo necesia inyecta un objeto
+//builder.Services AddTransient -> inyecta solo una vez y no inyecta nunca mas
+
 
 var app = builder.Build();
+
+//en program no se puede injectar solo en clases
+
+//creamosun metodo para injectar
+
+SeedData(app);
+
+void SeedData(WebApplication? app)
+{
+    //creamos una injection en mano
+  IServiceScopeFactory? scopeFactory= app.Services.GetService<IServiceScopeFactory>();
+    using (IServiceScope? scope = scopeFactory!.CreateScope())
+    {
+        SeedDb? service= scope.ServiceProvider.GetService<SeedDb>();
+        //llamamos metodo SeedAsync que es asycrono con metodo Wait()
+        service!.SeedAsync().Wait();
+    }
+}
+
+
+
+
 
 
 if (app.Environment.IsDevelopment())
